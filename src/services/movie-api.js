56ac26c5fb41a2e7c9fa34d1b57bfc4bbbd3f21f
Gema <gemovics@gmail.com>
2019-12-09 22:55:26 +0100

    Vuex store and services

---
+++ src/services/movie-api.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const QUERY_PARAMS_SEARCH = (title, type, year, page = 1) => `?apikey=f915e4d&s=${title}&type=${type}&y=${year}&page=${page}`
+const QUERY_PARAMS_MOVIE = (imdbID) => `?i=${imdbID}&apikey=f915e4d`
+class MovieService {
+    constructor() {
+        const instance = axios.create({
+            baseURL: 'http://www.omdbapi.com/',
+            headers: {Accept: 'application/json'}
+        });
+        
+        this.apiClient = instance;
+    }
+    async searchMoviesOnly(title, type, year) {
+        const { data } = await this.apiClient.get(QUERY_PARAMS_SEARCH(title, type, year));
+        return data.Search;
+    }
+    async searchMovies(params) {
+        const { data } = await this.apiClient.get(QUERY_PARAMS_SEARCH(params.title, params.type, params.year));
+        return data;
+    }
+    async loadMoreMovies(params, page) {
+        const { data }  = await this.apiClient.get(QUERY_PARAMS_SEARCH(params.title, params.type, params.year, page));
+        console.log(data)
+         return data.Search;
+    }
+    async getMovie(imdbID) {
+        const { data } = await this.apiClient.get(QUERY_PARAMS_MOVIE(imdbID));
+        return data;
+    }
+}
+const movieService = new MovieService();
+
+export default movieService;
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,78 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import moviesService from '../services/movie-api'
+
+Vue.use(Vuex);
 
 const state = () => ({
     topRated: [],
-    searchResult: [],
+    searchResults: [],
+    totalResults: 0,
+    pageNumber: 0,
+    currentPage: 0,
+    searchParams: {
+        title: "",
+        type: "",
+        year: "",
+    }
+    
     
 
 })
+const getters = {
+    getSearchResults: (state) => {
+        return state.searchResults
+    },
+    getTotalResults: (state) => {
+        return state.totalResults
+    },
+    getPageNumber: (state) => {
+        return state.pageNumber
+    },
+    getCurrentPage: (state) => {
+        return state.currentPage
+    },
+    getSearchParams: (state) => {
+        return state.searchParams
+    }
+}
+const actions = {
+    async searchMovies({commit},params){
+        const result = await moviesService.searchMovies(params)
+        const data = {params, result}
+        commit('SEARCH_MOVIES', data)
+        
+    },
+    async loadMoreMovies({commit}) {
+
+        if(this.state.currentPage < this.state.pageNumber) {
+            console.log('loadMoreMovies IF')
+            const nextPage = this.state.currentPage + 1
+            const result = await moviesService.loadMoreMovies({...this.state.searchParams}, nextPage)
+            commit('LOAD_MORE_MOVIES', result)          
+        }
+        
+    }
+
+}
+const mutations = {
+    SEARCH_MOVIES(state, {params, result}) {
+        state.searchResults = result.Search
+        state.totalResults = result.totalResults
+        state.totalResults = result.totalResults
+        //state.pageNumber = result.totalResults % 10 === 0?result.totalResults / 10:Math.floor(result.totalResults / 10) + 1
+
+        state.pageNumber = Number.isInteger(result.totalResults / 10)?result.totalResults / 10:Math.floor(result.totalResults / 10) + 1
+        state.currentPage = 1
+        state.searchParams = {...params}
+        console.log(state.searchParams)
+    },
+    LOAD_MORE_MOVIES(state, movies) {
+        state.searchResults.push(...movies )
+        state.currentPage++
+    }
+    
+}
 export default new Vuex.Store({
     state,
     getters,
